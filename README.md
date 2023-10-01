547e29bf60753f6f68f62f2b8638241583cba128
Johar Osorio <johar.osorio@intercapit.com>
2023-10-01 12:36:49 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# blog_aumenta_app_example
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/blog_aumenta_app_example/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.blog_aumenta_app_example
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
